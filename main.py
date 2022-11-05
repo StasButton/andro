907e2baa21d13341f0eac4c58f668aaf1e6bdc5b
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-05 15:55:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
-import os
-import uvicorn
-from fastapi import FastAPI,  File, UploadFile
+from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return "Hello Stas Fe"
-'''
-@app.post("/create_file/")
-async def image(image: UploadFile = File(...)):
-    try:
-        os.mkdir("images")
-    except Exception as e:
-        print(e)
-    file_name = os.getcwd()+"/images/"+image.filename.replace(" ", "-")
-    with open(file_name,'wb+') as f:
-        f.write(image.file.read())
-    return {"filename": file_name}
-'''
-
-if __name__ == "__main__":
-    uvicorn.run(app, host=0.0.0.0", port=8000)
+@app.get("/") 
+def hello():  
+    return {"message":"Hello TutLinks.com"}
