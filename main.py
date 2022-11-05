dbb60329f22ee08b14bbc97c49b02f733c95f603
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-06 01:15:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import os
-from fastapi import FastAPI,  File, UploadFile
+#from os import getcwd
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
-@app.get("/") 
-def hello():  
-    return {"message":"Hello TutLinks.com"}
+@app.get("/")
+async def read_root():
+    return {"mes":getcwd()}
+
 
 @app.post("/create_file/")
-def image(image: UploadFile = File(...)):
+async def image(image: UploadFile = File(...)):
     try:
         os.mkdir("images")
     except Exception as e:
@@ -16,5 +19,5 @@ def image(image: UploadFile = File(...)):
     file_name = os.getcwd()+"/images/"+image.filename.replace(" ", "-")
     with open(file_name,'wb+') as f:
         f.write(image.file.read())
-    return {"filename": file_name}
+    return {"p": os.getcwd()}
     
