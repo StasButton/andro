463b0ddbc880b1bce04e884baf70ecfa56d43065
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-06 21:42:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,52 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    dir = os.getcwd()
-    files = os.listdir(path=dir)
-    st = 'empty'
-    for i in files:
-        if i == 'images':
-            st = 'images'      
-    return {"mes":st}
+    dir = os.getcwd()+'/images/'
+    files = os.listdir(path=dir)        
+    return {"mes":files}
 
 
-@app.post("/create_file/")
+@app.post("/upload_file/")
 async def image(image: UploadFile = File(...)):
+    '''
     try:
         os.mkdir("images")
     except Exception as e:
         print(e)
+    '''
     file_name = os.getcwd()+"/images/"+image.filename.replace(" ", "-")
     with open(file_name,'wb+') as f:
         f.write(image.file.read())
     return {"p": os.getcwd()}
+
+
+from fastapi.responses import FileResponse
+#from os import getcwd, remove
+
+@app.get("/dl/{name_file}")
+def download_file(name_file: str):
+    return  FileResponse(path=os.getcwd() + "/images/" + name_file, media_type='application/octet-stream', filename=name_file)
+
+@app.get("/del/")  
+def dF():
+    dir = os.getcwd()+'/images/'
+    files = os.listdir(path=dir)
+
+    #os.remove(dir + files[0])
+
+     
+    for i in files:
+        try:
+            os.remove(dir + i)
+        except FileNotFoundError:
+            return 'remove fail'
     
+    
+    return files
+
+@app.post("/create_folder/")
+async def fold():
+    try:
+        os.mkdir("folder")
+    except Exception as e:
+        print(e)
