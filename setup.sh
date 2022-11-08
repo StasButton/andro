73da3c8e0f47627f639a0ccd806c185e8d1aceea
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-08 22:38:32 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"st.rubtsov@yandex.ru\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
