75a23fbf7433d1d842b6ebf623e95e26d5c38991
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-09 02:14:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,16 @@ from tensorflow.keras.preprocessing import image
 
 
 @st.cache(allow_output_mutation=True)
-'''
-def load_model():
+
+'''def load_model():
    #return EfficientNetB0(weights='imagenet')
-
-
 def preprocess_image(img):
     img = img.resize((224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
-    return x
-'''
+    return x'''
+
 
 def load_image():
     uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
@@ -30,24 +28,25 @@ def load_image():
     else:
         return None
 
-'''
-def print_predictions(preds):
+
+'''def print_predictions(preds):
     classes = decode_predictions(preds, top=3)[0]
     for cl in classes:
         st.write(cl[1], cl[2])
+#model = load_model()'''
 
-
-#model = load_model()
-'''
 
 st.title('Классификации изображений в облаке Streamlit')
 img = load_image()
-'''
-result = st.button('Распознать изображение')
+
+
+
+'''result = st.button('Распознать изображение')
 if result:
     x = preprocess_image(img)
     preds = model.predict(x)
     st.write('**Результаты распознавания:**')
-    print_predictions(preds)
-'''
+    print_predictions(preds)'''
+    
+
 
