2328d7120f47bdb0f85d5e9084c75946b778c8ad
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-09 02:28:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ import io
 import streamlit as st
 from PIL import Image
 import numpy as np
-#from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.preprocessing import image
-#from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 
 
 @st.cache(allow_output_mutation=True)
 
-#def load_model():
-   #return EfficientNetB0(weights='imagenet')
-#def preprocess_image(img):
- #   img = img.resize((224, 224))
-  #  x = image.img_to_array(img)
-   # x = np.expand_dims(x, axis=0)
-    #x = preprocess_input(x)
-    #return x
+def load_model():
+   return EfficientNetB0(weights='imagenet')
+def preprocess_image(img):
+    img = img.resize((224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
 
 
 def load_image():
@@ -29,11 +29,11 @@ def load_image():
         return None
 
 
-#def print_predictions(preds):
- #   classes = decode_predictions(preds, top=3)[0]
-  #  for cl in classes:
-   #     st.write(cl[1], cl[2])
-#model = load_model()
+def print_predictions(preds):
+    classes = decode_predictions(preds, top=3)[0]
+    for cl in classes:
+        st.write(cl[1], cl[2])
+model = load_model()
 
 
 st.title('Классификации изображений в облаке Streamlit')
