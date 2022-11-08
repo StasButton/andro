ac322009e4126eda337c96c349f59d5675491a1c
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-09 02:02:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-#import os
-#import uvicorn
-#from fastapi import FastAPI, File, UploadFile
-#from fastapi.responses import FileResponse
-
 import io
 import streamlit as st
 from PIL import Image
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 import numpy as np
+#from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.preprocessing import image
+#from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 
 
 @st.cache(allow_output_mutation=True)
-@st.cache(suppress_st_warning=True)
+'''
+def load_model():
+   #return EfficientNetB0(weights='imagenet')
+
+
+def preprocess_image(img):
+    img = img.resize((224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+'''
+
 def load_image():
     uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
     if uploaded_file is not None:
@@ -22,7 +30,24 @@ def load_image():
     else:
         return None
 
-st.title('Классификации изображений в облаке')
-img = load_image()
+'''
+def print_predictions(preds):
+    classes = decode_predictions(preds, top=3)[0]
+    for cl in classes:
+        st.write(cl[1], cl[2])
 
+
+#model = load_model()
+'''
+
+st.title('Классификации изображений в облаке Streamlit')
+img = load_image()
+'''
+result = st.button('Распознать изображение')
+if result:
+    x = preprocess_image(img)
+    preds = model.predict(x)
+    st.write('**Результаты распознавания:**')
+    print_predictions(preds)
+'''
 
