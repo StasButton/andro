521ca824639e70ae3cc28168e6a1cd67b5cec114
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-12 04:26:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from PIL import Image
 import numpy as np
 from tensorflow.keras.preprocessing import image
 
+from tensorflow.keras import utils
+#ar = np.random.randint(0, 255, (28,28,3))
+
+
 def load_image():
     uploaded_file = st.file_uploader(label='Выберите изображение')
     if uploaded_file is not None:
@@ -12,10 +16,11 @@ def load_image():
         img = Image.open(io.BytesIO(image_data))
         #
         ar = np.array(img)
+        img = utils.array_to_img(ar)
         
-        img_byte_arr = io.BytesIO()
-        roi_img.save(img_byte_arr, format='PNG')
-        #img_byte_arr = img_byte_arr.getvalue()
+        img_byte_arr = io.BytesIO(ar)
+        img.save(img_byte_arr, format='PNG')
+        image_data_arr = img.getvalue()
         
         #st.text(type(ar))
         return  image_data 
