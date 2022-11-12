6491539e2bbdd8025bde83c04d38fa3374f09845
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-12 04:03:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def load_image():
         #
         ar = np.array(img)
         st.text(type(ar))
-        return  s #image_data 
+        return  ar #image_data 
     else:
         return None
 
