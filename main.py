0336a176b0c91a6d0b26adcad2846ea45eea0982
StasButton <80902229+StasButton@users.noreply.github.com>
2022-12-16 23:47:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
-import io
+import android
 import streamlit as st
-from PIL import Image
-import numpy as np
-from tensorflow.keras.preprocessing import image
 
-from tensorflow.keras import utils
-#ar = np.random.randint(0, 255, (28,28,3))
+droid = android.Android()
 
-
-def load_image():
-    uploaded_file = st.file_uploader(label='Выберите изображение')
-    if uploaded_file is not None:
-        image_data = uploaded_file.getvalue()
-        st.image(image_data)
-        return image_data   
-    else:
-        return None
-
-st.title('Загрузка, скачивание изображений')
-
-s = load_image()
-if s is not None:
-    st.download_button(label='скачать',data=s,file_name = 'O.jpg')
-    #result = st.button('Распознать изображение')
-
-    
-
-
-
-
+droid.startLocating()
+#print "reading GPS ..."
+event = droid.eventWaitFor('location',10000).result
+if event['name'] == "location":
+  try:
+    lat = str(event['data']['gps']['latitude'])
+    lng = str(event['data']['gps']['longitude'])
+  except KeyError:
+    lat = str(event['data']['network']['latitude'])
+    lng = str(event['data']['network']['longitude'])    
+  latlng = 'lat: ' + lat + ' lng: ' + lng
+  st.text(latlng)
