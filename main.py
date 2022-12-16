e327c71a5d6c535fb9d8382e8ac00a7f62109e0e
StasButton <80902229+StasButton@users.noreply.github.com>
2022-12-17 00:03:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,4 @@ import streamlit as st
 
 #droid = android.Android()
 
-'''
-droid.startLocating()
-#print "reading GPS ..."
-event = droid.eventWaitFor('location',10000).result
-if event['name'] == "location":
-  try:
-    lat = str(event['data']['gps']['latitude'])
-    lng = str(event['data']['gps']['longitude'])
-  except KeyError:
-    lat = str(event['data']['network']['latitude'])
-    lng = str(event['data']['network']['longitude'])    
-  latlng = 'lat: ' + lat + ' lng: ' + lng
-  st.text(latlng)
-  '''
+
