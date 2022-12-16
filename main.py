ddf88ead703df36ad1fb468c27759c5b112266e8
StasButton <80902229+StasButton@users.noreply.github.com>
2022-12-17 01:27:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-#import android
 import streamlit as st
+from subprocess import Popen
 
-#droid = android.Android()
+def get_gps_coordinates() -> Coordinates:
+    """Returns current coordinates using MacBook GPS"""
+    process = Popen(["whereami"], stdout=PIPE)
+    (output, err) = process.communicate()
+    exit_code = process.wait()
+    if err is not None or exit_code != 0:
+        raise CantGetCoordinates
+    output_lines = output.decode().strip().lower().split("\n")
+    latitude = longitude = None
+    for line in output_lines:
+        if line.startswith("latitude:"):
+            latitude = float(line.split()[1])
+        if line.startswith("longitude:"):
+            longitude = float(line.split()[1])
+    return Coordinates(longitude=longitude, latitude=latitude)
 
 
